2e82620d048b6b0ad79decc8a8e2e9c8a13336bc
Ruslan Ka <octopused@gmail.com>
2024-02-12 12:36:54 +0500

    Name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "UseDeskSwift",
+    name: "UseDeskSwiftSPM",
     defaultLocalization: "en",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
